5ab715bc47ba44ed7fb9a4dc9ea520374680f6ff
Roman Prybluda <romanodua@gmail.com>
2023-02-20 15:13:26 +0200

    Add project files.

---
+++ 01_Calculator/Program.cs
@@ -0,0 +1,67 @@
+﻿
+using _01_Calculator;
+
+Console.Title = "___Simple calculator___";
+
+string reply;
+
+do
+{
+    Console.WriteLine("For addition       + input 1");
+    Console.WriteLine("For subtraction    - input 2");
+    Console.WriteLine("For division       / input 3");
+    Console.WriteLine("For multiplication * input 4");
+
+    Console.WriteLine("\nInput type operation:");
+    int operation = Convert.ToInt32(Console.ReadLine());
+
+    Console.Clear();
+
+    /*
+
+    string operationPrint;
+    {
+        if (operation == 1)
+        { operationPrint = "addition +"; }
+
+        if (operation == 2)
+        { operationPrint = "subtraction -"; }
+
+        if (operation == 3)
+        { operationPrint = "division /"; }
+
+        if (operation == 4)
+        { operationPrint = "multiplication *"; }
+
+    }
+        
+    //    Console.WriteLine($"Operation is: {operationPrint}");
+    
+   */
+
+    Console.WriteLine($"Operation is: ");
+
+    Console.WriteLine("Input X:");
+    int x = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Input Y:");
+    int y = Convert.ToInt32(Console.ReadLine());
+
+    int result = Result.CalcResult(operation, y, x);
+
+    Console.WriteLine($"Result: {result}");
+
+    Console.WriteLine("Do you want to continue? (y/n)");
+    reply = Console.ReadLine();
+
+
+
+    if (reply.ToLower() == "y")
+    {
+        Console.Clear();
+    }
+
+}
+
+while (reply.ToLower() == "y");
+
---
+++ 01_Calculator/Result.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Calculator
+{
+    public class Result
+
+    {
+
+        public static int CalcResult(int operation, int x, int y)
+        {
+            int result = 0;
+            
+            if (operation == 1)
+            {
+                result = x + y;
+            }
+
+            if (operation == 2)
+            {
+                result = x - y;
+            }
+
+            if (operation == 3)
+            {
+                result = x / y;
+            }
+
+            if (operation == 4)
+            {
+                result = x * y;
+            }
+
+            return result;
+        }
+
+
+    }
+}
